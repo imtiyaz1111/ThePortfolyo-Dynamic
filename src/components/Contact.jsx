e92469f8f5a977ae9d653eab97142a5fcab47ec7
Md Imtiyaz Alam <118051060+imtiyaz1111@users.noreply.github.com>
2024-03-24 01:44:14 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
                           <p className="text-white fs-4">
                             Adress <br />{" "}
                             <span className="fs-6">
-                              nothing
+                             Los Angeles , America
                             </span>{" "}
                           </p>
                         </div>
@@ -80,7 +80,7 @@ const Contact = () => {
                           <p className="text-white fs-4">
                             Phone <br />{" "}
                             <span className="fs-6">
-                             62200
+                             +91-9212321321
                             </span>{" "}
                           </p>
                         </div>
